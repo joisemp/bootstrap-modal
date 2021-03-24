a6f4946a6c5c622ce78a85e3f410cab60cb44dd1
joisemp <joise1124@gmail.com>
2021-03-24 18:58:05 +0530

    created requirements.txt and edited models.py file in pictures directory to automatically delete the uploaded image when deleting a model

--- pictures/models.py
+++ pictures/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
 
 class Picture(models.Model):
     item = models.ImageField(upload_to = 'images/')
@@ -7,3 +9,8 @@ class Picture(models.Model):
 
     def __str__(self):
         return str(self.pk)
+
+@receiver(pre_delete, sender=Picture)
+def Post_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.item.delete(False)
