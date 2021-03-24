235bf140822437468fbac22cfad55a35d3baf439
joisemp <joise1124@gmail.com>
2021-03-24 16:55:25 +0530

    Created an app called pictures and folder for static, templates and media

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
